ea5e09fcfc912ef89aa482697b6d810676cbeea4
priyanshi496 <132667193+priyanshi496@users.noreply.github.com>
2025-04-19 11:33:29 +0530

    committ

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ function showAppointment(event) {
     document.getElementById("modalName").textContent = document.getElementById("name").value;
     document.getElementById("modalEmail").textContent = document.getElementById("email").value;
     document.getElementById("modalPhone").textContent = document.getElementById("phone").value;
-    document.getElementById("modalService").textContent = document.getElementById("service").value;
+
+    let selectedOptions = Array.from(document.getElementById("service").selectedOptions).map(option => option.value);
+    document.getElementById("modalService").textContent = selectedOptions.join(", ");
+
     document.getElementById("modalDate").textContent = document.getElementById("appointmentDate").value;
 
     // Show Bootstrap Modal
     var myModal = new bootstrap.Modal(document.getElementById("appointmentModal"));
     myModal.show();
 }
+
     
     // Initiate the wowjs
     new WOW().init();
